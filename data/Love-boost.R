e53c8c3b5efc7efbb0fc441b863890372b9587a7
Thomas E. Love <tel3@case.edu>
2017-08-26 18:35:57 -0400

    Add zip file of all data and code.

--- data/Love-boost.R
+++ data/Love-boost.R
@@ -1,9 +1,10 @@
 `bootdif` <-
   function(y, g, conf.level=0.95, B.reps = 2000) {
+    require(Hmisc)
     lowq = (1 - conf.level)/2
     g <- as.factor(g)
-    a <- attr(Hmisc::smean.cl.boot(y[g==levels(g)[1]], B=B.reps, reps=TRUE),'reps')
-    b <- attr(Hmisc::smean.cl.boot(y[g==levels(g)[2]], B=B.reps, reps=TRUE),'reps')
+    a <- attr(smean.cl.boot(y[g==levels(g)[1]], B=B.reps, reps=TRUE),'reps')
+    b <- attr(smean.cl.boot(y[g==levels(g)[2]], B=B.reps, reps=TRUE),'reps')
     meandif <- diff(tapply(y, g, mean, na.rm=TRUE))
     a.b <- quantile(b-a, c(lowq,1-lowq))
     res <- c(meandif, a.b)
